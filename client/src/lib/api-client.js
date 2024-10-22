a076c53b91d310ca5132453c87e170f7ebd536e3
Nikhil Wakode <wakode333nikhil@gmail.com>
2024-10-22 15:24:46 +0530

    add backend server url in axios

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST = "http://localhost:8747";
+const HOST = process.env.VITE_SERVER_URL ||"http://localhost:8747";
 
  const apiClient = axios.create({
     baseURL:HOST ,
