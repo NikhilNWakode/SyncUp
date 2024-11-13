4ad7eb73243c72150ff97b2bcc446501225c9ac9
Nikhil Wakode <148487093+NikhilNWakode@users.noreply.github.com>
2024-11-13 22:06:09 +0530

    Update logo.jsx

--- client/src/pages/chats/components/logo.jsx
+++ client/src/pages/chats/components/logo.jsx
@@ -7,9 +7,9 @@ const SyncUpLogo = () => {
       <div className="relative translate-x-[-10px]">
         <span className="text-4xl font-extrabold tracking-tighter">
           <span className="bg-gradient-to-r lobster-regular from-purple-700 via-purple-500 to-white text-transparent bg-clip-text">
-            Fric
+            Sync
           </span>
-          <span className="text-white lobster-regular">tion</span>
+          <span className="text-white lobster-regular">Up</span>
         </span>
         <span className="absolute -bottom-1 left-0 w-full h-0.5 bg-gradient-to-r from-purple-700 via-purple-500 to-white"></span>
       </div>
